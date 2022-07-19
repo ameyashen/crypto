004011a66481736295c5b72360920e9a8c8f7b67
ameyashen <41130853+ameyashen@users.noreply.github.com>
2022-07-18 20:06:41 -0700

    Update helper.h

--- helper.h
+++ helper.h
@@ -2,17 +2,28 @@
 #include <iostream>
 
 struct buf_data {
-  unisgned int len;
-  void * buf;
+  unisgned int m_len;
+  void * m_buf;
   
   buf_data(len)
   {
-    buf = new unsigned char[len];
+    m_buf = new unsigned char[len];
+    m_len = len;
   }
   
   ~buf_data()
   {
-    delete[] buf;
+    delete[] m_buf;
+    m_len = 0;
+  }
+  
+  concatenate(const buf_data &buf2)
+  {
+    unsigned char * tmp = new char[m_len + buf2->m_len];
+    memcpy(tmp, m_buf, m_len);
+    memcpy(tmp + m_len, buf2->m_buf, buf2->m_len);
+    delete[] m_buf;
+    m_len += buf2->m_len2; 
   }
 };
 
